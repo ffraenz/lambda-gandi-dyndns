762bef8abd259bd16f12f9910971f99e00a2c789
Fränz Friederes <fraenz@frieder.es>
2019-08-23 22:01:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# lambda-gandi-dyndns
+
+Amazon AWS Lambda function forwarding IP address updates from routers (e.g. FRITZ!Box) to the [Gandi.net LiveDNS API](https://doc.livedns.gandi.net/) to provide a custom dynamic DNS service.
+
+## Setup
+
+- Create a Lambda function with the content of this repository and connect it to the API Gateway service.
+
+- Use `main.handle_update` as the function handler.
+
+- Configure the Lambda function with the environment variables listed below.
+
+- Configure the endpoints listed below in the API Gateway service and deploy the API.
+
+- Use the following `Update-URL` structure to configure the Amazon AWS Lambda endpoint as a custom dynamic DNS service on FRITZ!Box routers:
+
+  ```
+  https://example.execute-api.region.amazonaws.com/stage/name/update?domain=<domain>&ip=<ipaddr>&ip6=<ip6addr>
+  ```
+
+- Use a username, password and domain that matches the environment config.
+
+## Endpoints
+
+### GET /update
+
+Updates the IP address for the given dynamic DNS domain name.
+
+Parameters:
+
+- `ip` – New IPv4 address
+- `ip6` – New IPv6 address
+- `domain` – Dynamic DNS domain name; Needs to be whitelisted in the environment variables
+
+## Environment variables
+
+| Key                    | Description                                                  |
+| ---------------------- | ------------------------------------------------------------ |
+| `AUTH_USERNAME` | Basic authentication username required to fulfil requests   |
+| `AUTH_PASSWORD` | Basic authentication password required to fulfil requests   |
+| `GANDI_API_KEY`        | [Gandi.net API key](https://doc.livedns.gandi.net/)          |
+| `RECORD_TTL`           | Record time to live (TTL)                                    |
+| `DOMAIN_WHITELIST`     | Comma-separated (`,`) whitelist of domain names that may receive updates |
+
---
+++ main.py
@@ -0,0 +1,98 @@
+
+import json, os, base64
+from botocore.vendored import requests
+
+def handle_update(event, context):
+    """Update endpoint entrypoint"""
+
+    # Verify auth token
+    if not verify_auth(event['headers']['Authorization']):
+        print('Received request with invalid authorization token.')
+        return {
+            'statusCode': 403,
+            'body': json.dumps({ 'success': False })
+        }
+
+    # Collect update data
+    ip4_address = event['queryStringParameters']['ip']
+    ip6_address = event['queryStringParameters']['ip6']
+    domain = event['queryStringParameters']['domain']
+    domain_parts = domain.split('.') if domain else []
+
+    # Check wether this is a valid request
+    if (not ip4_address and not ip6_address) or len(domain_parts) < 2:
+        print('Received invalid request.')
+        return {
+            'statusCode': 400,
+            'body': json.dumps({ 'success': False })
+        }
+
+    # Verify that the requested domain is whitelisted
+    if not is_domain_whitelisted(domain):
+        print('The requested domain {} is not whitelisted.'.format(domain))
+        return {
+            'statusCode': 403,
+            'body': json.dumps({ 'success': False })
+        }
+
+    # Log request
+    print('Received request to set {} to {}.'.format(domain, ip4_address))
+
+    # Derive domain base and record name from domain parts
+    domain_base = '.'.join(domain_parts[-2:])
+    record_name = '.'.join(domain_parts[:-2]) if len(domain_parts) > 2 else '@'
+
+    # Update A (IPv4) and/or AAAA (IPv6) DNS records on base domain
+    if ip4_address:
+        update_record(domain_base, 'A', record_name, ip4_address)
+    if ip6_address:
+        update_record(domain_base, 'AAAA', record_name, ip6_address)
+
+    # Respond
+    return {
+        'statusCode': 200,
+        'body': json.dumps({ 'success': True })
+    }
+
+def verify_auth(authorization):
+    """Verifies the given authorization header value"""
+
+    username = os.environ['AUTH_USERNAME']
+    password = os.environ['AUTH_PASSWORD']
+    token = base64.b64encode('{}:{}'.format(username, password).encode())
+    return authorization == 'Basic ' + token.decode()
+
+def is_domain_whitelisted(domain):
+    """Verifies wether the given domain is whitelisted"""
+
+    whitelisted_domains = os.environ['DOMAIN_WHITELIST'].split(',')
+    return domain in whitelisted_domains
+
+def update_record(domain, record_type, name, value, ttl = None):
+    """Updates the given record"""
+
+    # Default record TTL to environment variable
+    ttl = ttl if ttl != None else os.environ['RECORD_TTL']
+
+    response = requests.request(
+        'PUT',
+        'https://dns.api.gandi.net/api/v5/domains/{}/records/{}/{}'.format(
+            domain,
+            name,
+            record_type
+        ),
+        data=json.dumps({
+            'rrset_ttl': ttl,
+            'rrset_values': [value]
+        }),
+        headers={
+            'Content-Type': 'application/json',
+            'X-Api-Key': os.environ['GANDI_API_KEY']
+        }
+    )
+
+    # Log result
+    print('Updated record {} {} {}.{} to value {} with status {}'.format(
+        record_type, ttl, name, domain, value, response.status_code))
+
+    return response.status_code >= 200 and response.status_code <= 299
